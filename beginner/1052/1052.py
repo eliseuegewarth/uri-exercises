a346465a80db96313eec864a7c9b3d036d4a7077
Eliseu Egewarth <eliseuegewarth@gmail.com>
2019-08-08 17:06:17 -0300

    Adicionando exercício #1052
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

---
+++ beginner/1052/1052.py
@@ -0,0 +1,16 @@
+
+months = ["",
+"January",
+"February",
+"March",
+"April",
+"May",
+"June",
+"July",
+"August",
+"September",
+"October",
+"November",
+"December"]
+x = int(input())
+print("{}".format(months[x]))
