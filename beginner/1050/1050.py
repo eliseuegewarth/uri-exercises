bff55f5d7efce139f2a930b05e0fb5c8e6097444
Eliseu Egewarth <eliseuegewarth@gmail.com>
2019-08-08 01:02:27 -0300

    Adicionando exercício #1050
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

---
+++ beginner/1050/1050.py
@@ -0,0 +1,21 @@
+
+x = int(input())
+
+if x == 11:
+	print("Sao Paulo")
+elif x == 19:
+	print("Campinas")
+elif x == 21:
+	print("Rio de Janeiro")
+elif x == 27:
+	print("Vitoria")
+elif x == 31:
+	print("Belo Horizonte")
+elif x == 32:
+	print("Juiz de Fora")
+elif x == 61:
+	print("Brasilia")
+elif x == 71:
+	print("Salvador")
+else:
+	print("DDD nao cadastrado")
